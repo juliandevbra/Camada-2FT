fa442c9a523097ec12b32e59cd2ee16cb6390c32
unknown <DH@DH-CARRERA-34.DH.digitalhouse.com>
2022-10-20 16:24:40 -0300

    Clase 3 - Repaso

--- src/Clase 3/App.jsx
+++ src/Clase 3/App.jsx
@@ -1,12 +1,25 @@
 import './App.css'
+import imagen from './assets/satisfied.png'
 
 function App() {
+
+  let headers = ['Home', 'Home', 'About', 'Contact', 'FAQ']
+
+  let users = [
+    {id: 0, name: 'Lucas', age: 25 },
+    {id: 1, name: 'Sofi', age: 27 },
+    {id: 2, name: 'Diego', age: 10 },
+  ]
 
   return (
     <div className="App">
       <h1>Hola pulsoneta 3.0!!!</h1>
-      <img src="/image/satisfied.png"/>
       <input/>
+      <ul>
+        {users.map((user) => {
+          return <li key={user.id}>{user.name}</li>
+        })}
+      </ul>
     </div>
   )
 }
