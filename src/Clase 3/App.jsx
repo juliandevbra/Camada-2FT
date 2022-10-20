58ce3a7bf34cf10231af337296b2890f728e63a7
unknown <DH@DH-CARRERA-34.DH.digitalhouse.com>
2022-10-20 16:23:38 -0300

    Clase 3 - Repaso

---
+++ src/Clase 3/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Hola pulsoneta 3.0!!!</h1>
+      <img src="/image/satisfied.png"/>
+      <input/>
+    </div>
+  )
+}
+
+export default App
