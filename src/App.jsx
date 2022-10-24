2035baf4e74555227cb75dbc22338ed1acca0090
unknown <DH@DH-CARRERA-34.DH.digitalhouse.com>
2022-10-24 16:52:07 -0300

    Clase 4 - Componentes y Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,13 @@
 import './App.css'
-import imagen from './assets/satisfied.png'
+import Home from './Pages/Home'
+import Shop from './Pages/Shop'
+
 
 function App() {
-
-  let headers = ['Home', 'Home', 'About', 'Contact', 'FAQ']
-
-  let users = [
-    {id: 0, name: 'Lucas', age: 25 },
-    {id: 1, name: 'Sofi', age: 27 },
-    {id: 2, name: 'Diego', age: 10 },
-  ]
-
   return (
     <div className="App">
-      <h1>Hola pulsoneta 3.0!!!</h1>
-      <input/>
-      <ul>
-        {users.map((user) => {
-          return <li key={user.id}>{user.name}</li>
-        })}
-      </ul>
+      <Home/>
+      <Shop/>
     </div>
   )
 }
---
+++ src/Components/FuncComponent.jsx
@@ -0,0 +1,6 @@
+
+const FuncComponent = () =>{
+    return <h1>Function Component</h1>
+}
+
+export default FuncComponent
